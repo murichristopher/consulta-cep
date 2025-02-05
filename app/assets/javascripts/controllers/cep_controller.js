445c9e515ec88e243ef55d7815ac449498cc798c
muri <murichristopher@outlook.com>
2025-02-05 16:37:14 -0300

    feat: add skeleton loading effect for CEP search

--- app/assets/javascripts/controllers/cep_controller.js
+++ app/assets/javascripts/controllers/cep_controller.js
@@ -1,14 +1,14 @@
+// app/javascript/controllers/cep_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input"]
+  static targets = ["input", "skeleton", "result"]
 
   format(event) {
     const input = event.target;
     let digits = input.value.replace(/\D/g, '');
 
     if (digits.length > 8) digits = digits.substring(0, 8);
-
     if (digits.length > 5) {
       input.value = `${digits.slice(0, 5)}-${digits.slice(5)}`;
     } else {
@@ -16,14 +16,8 @@ export default class extends Controller {
     }
   }
 
-  validate() {
-    const input = this.inputTarget;
-    const cepPattern = /^\d{5}-?\d{3}$/;
-
-    if (!cepPattern.test(input.value)) {
-      input.classList.add("input-error");
-    } else {
-      input.classList.remove("input-error");
-    }
+  showLoading() {
+    this.resultTargets.forEach(target => target.classList.add("hidden"));
+    this.skeletonTarget.classList.remove("hidden");
   }
 }
