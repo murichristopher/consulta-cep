a2ee5c5c093b5d22f50535fc5483b59246e2cd2b
muri <murichristopher@outlook.com>
2025-02-06 03:17:09 -0300

    feat(ui): enhance UI layout

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,20 +2,26 @@ class Address < ApplicationRecord
   validates :cep, presence: true, uniqueness: true
   validates :city, :state, presence: true
 
-scope :most_searched_ceps, ->(limit = 5) {
-  order(search_count: :desc)
-    .limit(limit) 
-    .pluck(:cep, :search_count)
-    .to_h
-}
+  scope :most_searched_ceps, ->(limit = 5) {
+    order(search_count: :desc)
+      .limit(limit)
+      .pluck(:cep, :search_count)
+      .to_h
+  }
 
   scope :ceps_by_state, ->(limit = 5) {
     group(:state).order(Arel.sql("COUNT(*) DESC")).limit(limit).count
   }
 
   scope :most_searched_by_location, ->(limit = 5) {
-    group(:city, :state).order(Arel.sql("COUNT(*) DESC")).limit(limit).count
+    group(:city, :state)
+      .order(Arel.sql("SUM(search_count) DESC"))
+      .limit(limit)
+      .sum(:search_count)
   }
+  def formatted_cep
+    cep.insert(5, "-")
+  end
 
   def increment_count
     self.class.update_counters(id, search_count: 1)
